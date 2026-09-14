8f5e4298c811512a7609718b7e29d74b30bf1659
Adam Bouqdib <adam@abemedia.co.uk>
2026-09-14 12:02:19 +0100

    chore: set up repository and add stagelint 0.1.7

--- default.nix
+++ default.nix
@@ -19,7 +19,7 @@
   # flakeModules = { }; # flake-parts modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  stagelint = pkgs.callPackage ./pkgs/stagelint { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
